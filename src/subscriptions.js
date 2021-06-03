38cfb4e6fa77639f07060f46e3ba60f3ba9b61ff
Matt Magoffin <git+matt@msqr.us>
2021-06-03 20:40:30 +1200

    Format source.

--- src/subscriptions.js
+++ src/subscriptions.js
@@ -11,44 +11,44 @@ var app;
  * @class
  * @param {Object} [options] optional configuration options
  */
-var samplerApp = function(options) {
+var samplerApp = function (options) {
   const self = { version: "0.1.0" };
-  const config = Object.assign(
-    {numMonths: 60},
-    options
-  );
+  const config = Object.assign({ numMonths: 60 }, options);
 
   /**
-   * A Map of tier key -> [{start:x, rate:y},...] array 
+   * A Map of tier key -> [{start:x, rate:y},...] array.
+   *
    * @type Map
    */
   var tiers;
 
   /**
    * The monthly costs table.
+   *
    * @type JQuery
    */
   var monthCostsTable;
 
   /**
    * The input form.
-   * 
+   *
    * @type HTMLFormElement
    */
   var form;
 
-  const numFormat = new Intl.NumberFormat('en-NZ');
-  const costFormat = new Intl.NumberFormat('en-NZ', {style:'currency', currency:'NZD'});
+  const numFormat = new Intl.NumberFormat("en-NZ");
+  const costFormat = new Intl.NumberFormat("en-NZ", { style: "currency", currency: "NZD" });
   var recalcTimer;
 
   /**
    * Get a configuration form field number.
+   *
    * @param {String} key the form element key
    * @param {Number} [val] the value to set
    * @returns {Number} the field value
    */
   function configurationNumber(key, val) {
-    if ( val !== undefined  ) {
+    if (val !== undefined) {
       form[key].value = val;
       return val;
     }
@@ -57,30 +57,30 @@ var samplerApp = function(options) {
 
   /**
    * Replace element contents with `data-tprop` attributes matching keys in a Map.
-   * 
+   *
    * @param {JQuery} el the element to replace template values on
    * @param {Map} data the data template values
    */
-	function replaceTemplateProperties(el, data) {
+  function replaceTemplateProperties(el, data) {
     var sel;
-		for ( let [k,v] of data ) {
+    for (let [k, v] of data) {
       sel = `[data-tprop='${k}']`;
       el.find(sel).addBack(sel).text(v);
     }
-	}
+  }
 
   /**
    * Calculate the cost of a usage against a set of tier rates.
-   * 
+   *
    * We assume the tiers are ordered, and start at 0.
-   * 
+   *
    * @param {Number} count the tier usage
    * @param {Array<Object>} tiers array of tier data with start/rate props (order assumed)
    */
   function calculateCost(count, tiers) {
     var cost = 0;
     var prevTier = tiers[0];
-    for ( let i = 1, len = tiers.length; i < len && count > 0; i += 1 ) {
+    for (let i = 1, len = tiers.length; i < len && count > 0; i += 1) {
       let tier = tiers[i];
       let tierCount = Math.min(count, tier.start);
       cost += tierCount * prevTier.rate;
@@ -92,81 +92,92 @@ var samplerApp = function(options) {
   }
 
   function recalc() {
-    const templateRow = monthCostsTable.children('thead').children('tr.template');
-    const tbody = monthCostsTable.children('tbody');
-    
-    const nodeCount = configurationNumber('nodeCount');
-    const sourcesPerNodeCount = configurationNumber('sourcesPerNodeCount');
-    const datumPerSourcePerHourCount = configurationNumber('datumPerSourcePerHourCount');
-    const propertiesPerDatumCount = configurationNumber('propertiesPerDatumCount');
-    const queriedDatumPerSourcePerHourCount = configurationNumber('queriedDatumPerSourcePerHourCount');
+    const templateRow = monthCostsTable.children("thead").children("tr.template");
+    const tbody = monthCostsTable.children("tbody");
 
-    const datumPerHourCount = configurationNumber('datumPerHourCount', nodeCount * sourcesPerNodeCount * datumPerSourcePerHourCount);
-    const propertiesPerHourCount = configurationNumber('propertiesPerHourCount', datumPerHourCount * propertiesPerDatumCount);
+    const nodeCount = configurationNumber("nodeCount");
+    const sourcesPerNodeCount = configurationNumber("sourcesPerNodeCount");
+    const datumPerSourcePerHourCount = configurationNumber("datumPerSourcePerHourCount");
+    const propertiesPerDatumCount = configurationNumber("propertiesPerDatumCount");
+    const queriedDatumPerSourcePerHourCount = configurationNumber(
+      "queriedDatumPerSourcePerHourCount"
+    );
 
-    const hoursPerMonth = 24 * 365 / 12;
-    
+    const datumPerHourCount = configurationNumber(
+      "datumPerHourCount",
+      nodeCount * sourcesPerNodeCount * datumPerSourcePerHourCount
+    );
+    const propertiesPerHourCount = configurationNumber(
+      "propertiesPerHourCount",
+      datumPerHourCount * propertiesPerDatumCount
+    );
+
+    const hoursPerMonth = (24 * 365) / 12;
+
     const propInCountPerMonth = propertiesPerHourCount * hoursPerMonth;
-    const propInCostPerMonth = calculateCost(propInCountPerMonth, tiers.get('datum-props-in'));
-    const datumQueriedPerMonth = sourcesPerNodeCount * queriedDatumPerSourcePerHourCount * hoursPerMonth;
-    const datumQueriedCostPerMonth = calculateCost(datumQueriedPerMonth, tiers.get('datum-out'));
+    const propInCostPerMonth = calculateCost(propInCountPerMonth, tiers.get("datum-props-in"));
+    const datumQueriedPerMonth =
+      sourcesPerNodeCount * queriedDatumPerSourcePerHourCount * hoursPerMonth;
+    const datumQueriedCostPerMonth = calculateCost(datumQueriedPerMonth, tiers.get("datum-out"));
 
     const rowData = new Map();
-    rowData.set('propInCount', numFormat.format(propInCountPerMonth));
-    rowData.set('datumQueriedCount', numFormat.format(datumQueriedPerMonth));
-    rowData.set('propInCost', costFormat.format(propInCostPerMonth));
-    rowData.set('datumQueriedCost', costFormat.format(datumQueriedCostPerMonth));
-
+    rowData.set("propInCount", numFormat.format(propInCountPerMonth));
+    rowData.set("datumQueriedCount", numFormat.format(datumQueriedPerMonth));
+    rowData.set("propInCost", costFormat.format(propInCostPerMonth));
+    rowData.set("datumQueriedCost", costFormat.format(datumQueriedCostPerMonth));
 
     var runningTotalCost = 0;
-    
+
     tbody.empty();
-    for ( let monthNum = 1, len = config.numMonths; monthNum <= len; monthNum += 1 ) {
-      rowData.set('year', Math.ceil(monthNum / 12));
-      rowData.set('month', monthNum);
-      
+    for (let monthNum = 1, len = config.numMonths; monthNum <= len; monthNum += 1) {
+      rowData.set("year", Math.ceil(monthNum / 12));
+      rowData.set("month", monthNum);
+
       let datumDaysStoredCount = Math.floor(
-        (datumPerHourCount * hoursPerMonth * monthNum) // raw
-        + (hoursPerMonth * monthNum) // hour agg
-        + (hoursPerMonth / 24 * monthNum) // day agg
-        + monthNum); // month add
-      rowData.set('datumDaysStoredCount', numFormat.format(datumDaysStoredCount));
+        datumPerHourCount * hoursPerMonth * monthNum + // raw
+          hoursPerMonth * monthNum + // hour agg
+          (hoursPerMonth / 24) * monthNum + // day agg
+          monthNum // month add
+      );
+      rowData.set("datumDaysStoredCount", numFormat.format(datumDaysStoredCount));
 
-      let datumDaysStoredCost = calculateCost(datumDaysStoredCount, tiers.get('datum-days-stored'));
-      rowData.set('datumDaysStoredCost', costFormat.format(datumDaysStoredCost));
+      let datumDaysStoredCost = calculateCost(datumDaysStoredCount, tiers.get("datum-days-stored"));
+      rowData.set("datumDaysStoredCost", costFormat.format(datumDaysStoredCost));
 
-      let monthCost = Number(Number(propInCostPerMonth + datumQueriedCostPerMonth + datumDaysStoredCost).toFixed(2));
-      rowData.set('monthCost', costFormat.format(monthCost));
-      
+      let monthCost = Number(
+        Number(propInCostPerMonth + datumQueriedCostPerMonth + datumDaysStoredCost).toFixed(2)
+      );
+      rowData.set("monthCost", costFormat.format(monthCost));
+
       runningTotalCost += monthCost;
-      rowData.set('runningTotalCost', costFormat.format(runningTotalCost));
+      rowData.set("runningTotalCost", costFormat.format(runningTotalCost));
 
       let row = templateRow.clone(true);
-      row.removeClass('template');
+      row.removeClass("template");
       replaceTemplateProperties(row, rowData);
-      if ( monthNum % 12 === 0 ) {
-        row.addClass('year-end');
-      } else if ( monthNum > 1 ) {
-        row.addClass('hidden');
+      if (monthNum % 12 === 0) {
+        row.addClass("year-end");
+      } else if (monthNum > 1) {
+        row.addClass("hidden");
       }
       tbody.append(row);
     }
   }
 
   function subscriptionName(key) {
-    if ( key === 'datum-props-in' ) {
-      return 'Properties posted';
-    } else if ( key === 'datum-out' ) {
-      return 'Datum queried';
+    if (key === "datum-props-in") {
+      return "Properties posted";
+    } else if (key === "datum-out") {
+      return "Datum queried";
     } else {
-      return 'Datum days stored';
+      return "Datum days stored";
     }
   }
 
   function subscriptionMillionsBase(key) {
-    if ( key === 'datum-props-in' ) {
+    if (key === "datum-props-in") {
       return 1;
-    } else if ( key === 'datum-out' ) {
+    } else if (key === "datum-out") {
       return 10;
     } else {
       return 100;
@@ -174,31 +185,34 @@ var samplerApp = function(options) {
   }
 
   function setupSubscriptionRatesTable(table) {
-    const templateRow = table.children('thead').children('tr.template');
-    const tbody = table.children('tbody');
+    const templateRow = table.children("thead").children("tr.template");
+    const tbody = table.children("tbody");
     const rowData = new Map();
-    for ( let [key, tierData] of tiers ) {
-      rowData.set('subscriptionName', subscriptionName(key));
+    for (let [key, tierData] of tiers) {
+      rowData.set("subscriptionName", subscriptionName(key));
       const millionsBase = subscriptionMillionsBase(key);
-      for ( let i = 0, len = tierData.length; i < len; i += 1 ) {
+      for (let i = 0, len = tierData.length; i < len; i += 1) {
         const tier = tierData[i];
-        const nextTier = (i+1 < len ? tierData[i+1] : undefined);
+        const nextTier = i + 1 < len ? tierData[i + 1] : undefined;
 
-        rowData.set('name', `${i+1}`);
-        rowData.set('start', numFormat.format(tier.start));
-        rowData.set('rate', `${costFormat.format(tier.rate * millionsBase * 1000000)} / ${millionsBase} million`);
-        
-        if ( nextTier) {
-          let maxCount = (nextTier.start - tier.start);
-          rowData.set('maximumCount', numFormat.format(maxCount));
-          rowData.set('maximumCost', costFormat.format(maxCount * tier.rate));
+        rowData.set("name", `${i + 1}`);
+        rowData.set("start", numFormat.format(tier.start));
+        rowData.set(
+          "rate",
+          `${costFormat.format(tier.rate * millionsBase * 1000000)} / ${millionsBase} million`
+        );
+
+        if (nextTier) {
+          let maxCount = nextTier.start - tier.start;
+          rowData.set("maximumCount", numFormat.format(maxCount));
+          rowData.set("maximumCost", costFormat.format(maxCount * tier.rate));
         } else {
-          rowData.delete('maximumCount');
-          rowData.delete('maximumCost');
+          rowData.delete("maximumCount");
+          rowData.delete("maximumCost");
         }
 
         let row = templateRow.clone(true);
-        row.removeClass('template');
+        row.removeClass("template");
         replaceTemplateProperties(row, rowData);
         tbody.append(row);
       }
@@ -207,13 +221,13 @@ var samplerApp = function(options) {
 
   function toggleShowAllMonths() {
     let btn = $(this);
-    let showAllMonths = btn.hasClass('years');
-    if ( showAllMonths ) {
-      monthCostsTable.find('tbody tr.hidden').removeClass('hidden');
+    let showAllMonths = btn.hasClass("years");
+    if (showAllMonths) {
+      monthCostsTable.find("tbody tr.hidden").removeClass("hidden");
     } else {
-      monthCostsTable.find('tbody tr:nth-child(n+2)').not('.year-end').addClass('hidden');
+      monthCostsTable.find("tbody tr:nth-child(n+2)").not(".year-end").addClass("hidden");
     }
-    btn.toggleClass('years', !showAllMonths);
+    btn.toggleClass("years", !showAllMonths);
     return false;
   }
 
@@ -226,38 +240,38 @@ var samplerApp = function(options) {
   }
 
   function init() {
-    monthCostsTable = $('#monthly-costs');
-    form = document.getElementById('configuration');
+    monthCostsTable = $("#monthly-costs");
+    form = document.getElementById("configuration");
 
     // craete map of tier -> [data]
     tiers = new Map();
-    options.tierData.forEach(row => {
+    options.tierData.forEach((row) => {
       let key = row[0];
       let data = tiers.get(key);
-      if ( !data ) {
+      if (!data) {
         data = [];
         tiers.set(key, data);
       }
-      data.push(Object.freeze({start:row[1], rate:row[2]}));
+      data.push(Object.freeze({ start: row[1], rate: row[2] }));
     });
 
     // listen for changes on all non-derived form elements to recalculate output
-    $('input:not(.derived)').on('keyup change', function() {
+    $("input:not(.derived)").on("keyup change", function () {
       // add a small delay after each keypress before actually attempting to recalculate
-      if ( recalcTimer ) {
+      if (recalcTimer) {
         clearTimeout(recalcTimer);
       }
       recalcTimer = setTimeout(recalc, 500);
     });
 
-    $('#toggle-years-only').on('click', toggleShowAllMonths);
+    $("#toggle-years-only").on("click", toggleShowAllMonths);
 
-    setupSubscriptionRatesTable($('#tier-rates'));
+    setupSubscriptionRatesTable($("#tier-rates"));
     recalc();
 
     return Object.defineProperties(self, {
       start: { value: start },
-      stop: { value: stop }
+      stop: { value: stop },
     });
   }
 
@@ -267,23 +281,23 @@ var samplerApp = function(options) {
 export default function startApp() {
   var config = {};
   config.tierData = [
-    ['datum-props-in', 0, 0.000005],
-    ['datum-props-in', 500000, 0.000003],
-    ['datum-props-in', 10000000, 0.0000008],
-    ['datum-props-in', 500000000, 0.0000002],
-    ['datum-out', 0, 0.0000005],
-    ['datum-out', 1000000, 0.0000002],
-    ['datum-out', 100000000, 0.00000005],
-    ['datum-out', 10000000000, 0.000000008],
-    ['datum-days-stored', 0, 0.00000005],
-    ['datum-days-stored', 10000000, 0.00000001],
-    ['datum-days-stored', 1000000000, 0.000000004],
-    ['datum-days-stored', 100000000000, 0.000000001],
+    ["datum-props-in", 0, 0.000005],
+    ["datum-props-in", 500000, 0.000003],
+    ["datum-props-in", 10000000, 0.0000008],
+    ["datum-props-in", 500000000, 0.0000002],
+    ["datum-out", 0, 0.0000005],
+    ["datum-out", 1000000, 0.0000002],
+    ["datum-out", 100000000, 0.00000005],
+    ["datum-out", 10000000000, 0.000000008],
+    ["datum-days-stored", 0, 0.00000005],
+    ["datum-days-stored", 10000000, 0.00000001],
+    ["datum-days-stored", 1000000000, 0.000000004],
+    ["datum-days-stored", 100000000000, 0.000000001],
   ];
 
   app = samplerApp(config).start();
 
-  window.onbeforeunload = function() {
+  window.onbeforeunload = function () {
     app.stop();
   };
 
